f57d07df0af133e959151f7c9d5b3f6f8af49127
Keylane <keylanessilva07@gmail.com>
2022-02-01 01:18:44 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -15,5 +15,11 @@ function apagaUm()
 function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = eval(resultado);
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }else
+    {
+        document.getElementById('resultado').innerHTML = "NADA..."
+    }
 }
